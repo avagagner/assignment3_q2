0013a001efed1433e895a72b6f7edd55eca0fa69
avagagner <46656654+avagagner@users.noreply.github.com>
2022-02-07 16:22:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,61 @@
-var mymap = L.map('mapid').setView([38.63775922635261, -90.28368046396037], 13);
-//https://a.tile.openstreetmap.org/{z}/{x}/{y}.png
-//'http://a.tile.stamen.com/toner/{z}/{x}/{y}.png'
-//https://wiki.openstreetmap.org/wiki/Tile_servers
-L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	subdomains: 'abcd',
-	minZoom: 1,
-	maxZoom: 16,
-	ext: 'jpg'
-}).addTo(mymap);
+    require([
+      "esri/Map",
+      "esri/layers/CSVLayer",
+      "esri/views/MapView",
+      "esri/config",
+      "esri/core/urlUtils",
+      "dojo/domReady!"
+    ], function(
+      Map,
+      CSVLayer,
+      MapView,
+      esriConfig,
+      urlUtils
+    ) {
 
-var marker = L.marker([38.63672876590212, -90.24674730564345]).addTo(mymap);
+      // If CSV files are not on the same domain as your website, a CORS enabled server
+      // or a proxy is required.
+    var url = "https://raw.githubusercontent.com/gbrunner/adv-programming-for-gis-and-rs/master/Web%20Development%20Module/Unit%202%20-%20ArcGIS%20JavaScript%20API/stl_crime_wgs_84.csv";
+     esriConfig.request.corsEnabledServers.push('https://rawgit.com');
 
-var marker2 = L.marker([38.63816176328897, -90.25104626536422]).addTo(mymap);
+      // Paste the url into a browser's address bar to download and view the attributes
+      // in the CSV file. These attributes include:
+      // * mag - magnitude
+      // * type - earthquake or other event such as nuclear test
+      // * place - location of the event
+      // * time - the time of the event
 
-var marker3 = L.marker([38.661539699331904, -90.30894447950988]).addTo(mymap);
+        const template = {
+          title: "Earthquake Info",
+          content: "Magnitude {mag} {type} hit {place} on {time}."
+        };
 
-var marker4 = L.marker([38.61714841986633, -90.27511168787689]).addTo(mymap);
+        const csvLayer = new CSVLayer({
+          url: url,
+          copyright: "USGS Earthquakes",
+          popupTemplate: template
+        });
 
-var marker5 = L.marker([38.64751484426265, -90.2744901289432]).addTo(mymap);
+        var symbol = {
+          type: "simple-marker", 
+          color: "#013220"
+        };
 
+      csvLayer.renderer = {
+        type: "simple", // autocasts as new SimpleRenderer()
+        symbol: symbol
+      };
 
+      var map = new Map({
+        basemap: "gray",
+        layers: [csvLayer]
+      });
 
-marker.bindPopup("<b>Scottish Arms</b>.").openPopup();
-marker2.bindPopup("<b>Scarlett's Wine Bar</b>.").openPopup();
-marker3.bindPopup("<b>Mi Ranchito</b>.").openPopup();
-marker4.bindPopup("<b>Lorenzo's Trattoria</b>.").openPopup();
-marker4.bindPopup("<b>2Shae Cafe</b>.").openPopup();
+      var view = new MapView({
+        container: "viewDiv",
+        center: [-90.194, 38.6270],
+        zoom: 11,
+        map: map
+      });
+
+    });
